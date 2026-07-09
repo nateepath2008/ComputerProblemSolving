022904c43a291562bc47581be9d678030124363b
nateepath2008 <nateepath.2008@gmail.com>
2026-07-09 15:43:59 +0700

    Ex05

--- Total.java
+++
@@ -1,8 +0,0 @@
-void main() {
-    IO.print("Enter Hours:");
-    int hours = Integer.parseInt(IO.readln());
-    IO.print("Enter Minter: ");
-    int mintes = Integer.parseInt(IO.readln());
-    int totlaMintes = (hours * 60) + mintes;
-    IO.println("Toteger mintes = " + totlaMintes);
-}
